ceb40b9a1715a0bf85e1372041f61cefa86a02db
도우진 <dwj1996@naver.com>
2019-08-24 01:11:15 +0900

    Solve the problems in baekjoon
     - Not solved : 1759, 2108, 2164, 2178, 2448, 2751, 6064, 9020, 10828
     - The number of solved:  121
     - Total Rank: 6593th
     - Rank in GNU: 16th

---
+++ baekjoon/2444py/a.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n-1-i):
+        print(" ", end="")
+    for j in range(2*(i+1)-1):
+        print("*", end="")
+    print()
+    
+for i in range(1, n):
+    for j in range(i):
+        print(" ", end="")
+    for j in range(2*((n-i))-1):
+        print("*", end="")
+    print()
